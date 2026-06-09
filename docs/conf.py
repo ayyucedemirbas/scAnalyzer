19caf7d83de759a49f8cb84e9679cbbcf3767766
Ayyuce Demirbas <a.ayyuced@gmail.com>
2026-06-09 08:05:03 +0300

    Add autodoc_mock_imports to conf.py
    Added autodoc_mock_imports for documentation generation.

--- docs/conf.py
+++ docs/conf.py
@@ -3,3 +3,19 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 html_theme = 'sphinx_rtd_theme'
+
+autodoc_mock_imports = [
+    "numpy", 
+    "pandas", 
+    "scipy", 
+    "sklearn", 
+    "statsmodels", 
+    "matplotlib", 
+    "seaborn", 
+    "h5py", 
+    "umap", 
+    "leidenalg", 
+    "louvain", 
+    "igraph", 
+    "plotly"
+]
