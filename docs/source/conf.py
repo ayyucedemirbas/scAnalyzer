f3845e75b62e6a4f0e34146f90f6e6f159e193d2
ayyucedemirbas <a.ayyuced@gmail.com>
2026-06-09 08:15:56 +0300

    docs requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,24 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../..'))
+
+autodoc_mock_imports = [
+    "numpy", 
+    "pandas", 
+    "scipy", 
+    "sklearn", 
+    "statsmodels", 
+    "matplotlib", 
+    "seaborn", 
+    "h5py", 
+    "umap", 
+    "leidenalg", 
+    "louvain", 
+    "igraph", 
+    "plotly"
+]
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -14,15 +35,18 @@ release = '0.2.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'myst_parser'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
